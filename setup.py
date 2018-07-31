1733d37e2102577e54131d836f50928270dd810d
muelleph <philipp.mueller@merkur.uni-freiburg.de>
2018-07-31 15:20:51 +0200

    MAINT removed 'sphinx' from setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ setup(
     classifiers=['Development Status :: 4 - Beta'],
     packages=find_packages(),
     python_requires='>=3',
-    install_requires=['Pyro4', 'serpent', 'ConfigSpace', 'numpy', 'statsmodels', 'scipy', 'sphinx', 'sphinx_rtd_theme',
-                      'sphinx_gallery'],
+    install_requires=['Pyro4', 'serpent', 'ConfigSpace', 'numpy', 'statsmodels', 'scipy'],
     extras_require={
         'Automatic hostname inference': ['netifaces'],
     },
